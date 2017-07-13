9cc79827ee71ec66d12eefb21f132e1bb04b639e
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-07-13 10:13:27 -0400

    added logic for 9pixel brush but not yet hooked up to anything

--- js/newCanvasModal.js
+++ js/newCanvasModal.js
@@ -1,13 +1,61 @@
 function paint() {
-  $('#paintTable td').on('mouseover', function(e) {
+  $('#paintTable td').on('mousedown', function(event) {
+    event.stopPropagation()
     $(this).css('background-color', `#${color}`)
-    e.stopPropagation()
+
+    $('#paintTable td').on('mouseover', function(e) {
+      $(this).css('background-color', `#${color}`)
+    })
+  })
+}
+
+function paint9() {
+  $('#paintTable td').on('mousedown', function(event) {
+    event.stopPropagation()
+    $(this).css('background-color', `#${color}`)
+    //attempt at enalarging brush serialize
+    let tdClass = parseInt(this.className)
+    let trClass = parseInt(this.parentElement.className)
+      // //sides of current selected td
+      $(`tr.${trClass} td.${tdClass-1}`).css('background-color', `#${color}`)
+      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `#${color}`)
+      //above current selected td
+      $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', `#${color}`)
+      $(`tr.${trClass-1} td.${tdClass}`).css('background-color', `#${color}`)
+      $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', `#${color}`)
+      //under current selected td
+      $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', `#${color}`)
+      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `#${color}`)
+      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `#${color}`)
+    // end attempt enlarging brush size
+
+    $('#paintTable td').on('mouseover', function(e) {
+      $(this).css('background-color', `#${color}`)
+
+      //attempt at enalarging brush serialize
+      let tdClass = parseInt(this.className)
+      let trClass = parseInt(this.parentElement.className)
+        // //sides of current selected td
+        $(`tr.${trClass} td.${tdClass-1}`).css('background-color', `#${color}`)
+        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `#${color}`)
+        //above current selected td
+        $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', `#${color}`)
+        $(`tr.${trClass-1} td.${tdClass}`).css('background-color', `#${color}`)
+        $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', `#${color}`)
+        //under current selected td
+        $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', `#${color}`)
+        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `#${color}`)
+        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `#${color}`)
+      // end attempt enlarging brush size
+      e.stopPropagation()
+    })
   })
 }
 
 function setPaintListener() {
-  $('#paintTable').on('mousedown', function(event) {
+  $('#paintTable').on('mouseover', function(event) {
     paint()
+    console.log('ready to paint!')
     event.stopPropagation()
   })
   $('#paintTable').on('mouseup', function(event) {
@@ -21,9 +69,9 @@ function createTable(x, y, pixelSize) {
 
   let t = "<table id='paintTable'><thead></thead><tbody>"
   for(let i=0; i<y; i++) {
-    t += "<tr>"
+    t += `<tr class='${i}'>`
     for(let j=0; j<x; j++) {
-      t += "<td></td>"
+      t += `<td class='${j}'></td>`
     }
     t += "</tr>"
   }
