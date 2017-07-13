53f8ba365fcd361788a1aa0fbbc5c11f3c2a15fb
Jason Sears <jsears4585@gmail.com>
2017-07-13 12:33:34 -0400

    Built eyedropper

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,6 @@
 //color stuff
 globalColor = '#00ff02'
+globalEye = 'off'
 
 // Make actually toggle
 $('#gridToggle').on('click', function(event) {
@@ -12,9 +13,41 @@ $('#showSaveModal').on('click', function(event){
   $('.ui.basic.modal.save-canvas').modal('show')
 })
 
-$(".basic").spectrum({
-  color: "#00ff02",
+$('.basic').spectrum({
+  color: '#00ff02',
   change: function(color) {
     globalColor = color.toHexString()
   }
-});
+})
+
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
+  function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2)
+  }
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3])
+}
+
+function eyedrop() {
+  $('table td').on('click', function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    globalColor = rgb2hex( $(this).css('backgroundColor') )
+    $('.sp-preview-inner').css('backgroundColor', globalColor)
+  })
+}
+
+$('#eyedropper').on('click', function(event) {
+  event.preventDefault()
+  $(this).toggleClass('active')
+  globalEye === 'off' ? globalEye = 'on' : globalEye = 'off'
+  if (globalEye === 'on') {
+    $('table').css('cursor','url(http://icons.iconarchive.com/icons/designcontest/outline/16/Eyedropper-icon.png), auto')
+    $('table').off()
+    eyedrop()
+  } else {
+    $('table').css('cursor','default')
+    $('table td').off()
+    setPaintListener()
+  }
+})
