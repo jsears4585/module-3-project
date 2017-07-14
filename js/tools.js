108423bf0ccccccf699040ca9467f8c74437c3c2
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-07-14 12:59:02 -0400

    deduped brush and eraser code, made it so that page brush1 selected  on load

--- js/tools.js
+++ js/tools.js
@@ -17,108 +17,119 @@ function rgb2hex(rgb) {
   return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3])
 }
 
-//PAINT / BRUSH
+//SHARED ERASER AND BRUSH CODE
+function brushPrep(action, size){
+  $('.brushSizes').removeClass('active')
+  $('.dependentTools').removeClass('active')
+  $(`#${action}MenuButton`).addClass('active')
+  $('#paintTable td').off()
+  $(`#${action}${size}`).toggleClass('active')
+  $('#paintTable').css('cursor',`url('css/cursors/${action}_cursor.png'), auto`)
+}
 
-function paint1() {
+function brush1(color) {
   $('#paintTable td').on('mousedown', function(event) {
     event.stopPropagation()
-    $(this).css('background-color', `${globalColor}`)
+    $(this).css('background-color', `${color}`)
 
     $('#paintTable td').on('mouseover', function(e) {
-      $(this).css('background-color', `${globalColor}`)
+      $(this).css('background-color', `${color}`)
     })
   })
   $('#paintTable td').on('mouseup', function(event) {
     $('#paintTable td').off()
-    paint1()
+    brush1()
     event.stopPropagation()
   })
 }
 
-function paint4() {
+function brush4(color) {
   $('#paintTable td').on('mousedown', function(event) {
     event.stopPropagation()
-    $(this).css('background-color', `${globalColor}`)
+    $(this).css('background-color', `${color}`)
     //attempt at enalarging brush serialize
     let tdClass = parseInt(this.className)
     let trClass = parseInt(this.parentElement.className)
       // //sides of current selected td
-      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${color}`)
       //under current selected td
-      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${globalColor}`)
-      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${color}`)
+      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${color}`)
     // end attempt enlarging brush size
 
     $('#paintTable td').on('mouseover', function(e) {
-      $(this).css('background-color', `${globalColor}`)
+      $(this).css('background-color', `${color}`)
       //attempt at enalarging brush serialize
       let tdClass = parseInt(this.className)
       let trClass = parseInt(this.parentElement.className)
         // //sides of current selected td
-        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${color}`)
         //under current selected td
-        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${globalColor}`)
-        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${color}`)
+        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${color}`)
       // end attempt enlarging brush size
       e.stopPropagation()
     })
   })
   $('#paintTable td').on('mouseup', function(event) {
     $('#paintTable td').off()
-    paint4()
+    brush4()
     event.stopPropagation()
   })
 }
 
-function paint9() {
+function brush9(color) {
   $('#paintTable td').on('mousedown', function(event) {
     event.stopPropagation()
-    $(this).css('background-color', `${globalColor}`)
+    $(this).css('background-color', `${color}`)
     //attempt at enalarging brush serialize
     let tdClass = parseInt(this.className)
     let trClass = parseInt(this.parentElement.className)
       // //sides of current selected td
-      $(`tr.${trClass} td.${tdClass-1}`).css('background-color', `${globalColor}`)
-      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+      $(`tr.${trClass} td.${tdClass-1}`).css('background-color', `${color}`)
+      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${color}`)
       //above current selected td
-      $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', `${globalColor}`)
-      $(`tr.${trClass-1} td.${tdClass}`).css('background-color', `${globalColor}`)
-      $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+      $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', `${color}`)
+      $(`tr.${trClass-1} td.${tdClass}`).css('background-color', `${color}`)
+      $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', `${color}`)
       //under current selected td
-      $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', `${globalColor}`)
-      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${globalColor}`)
-      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+      $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', `${color}`)
+      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${color}`)
+      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${color}`)
     // end attempt enlarging brush size
 
     $('#paintTable td').on('mouseover', function(e) {
-      $(this).css('background-color', `${globalColor}`)
+      $(this).css('background-color', `${color}`)
 
       //attempt at enalarging brush serialize
       let tdClass = parseInt(this.className)
       let trClass = parseInt(this.parentElement.className)
         // //sides of current selected td
-        $(`tr.${trClass} td.${tdClass-1}`).css('background-color', `${globalColor}`)
-        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+        $(`tr.${trClass} td.${tdClass-1}`).css('background-color', `${color}`)
+        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', `${color}`)
         //above current selected td
-        $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', `${globalColor}`)
-        $(`tr.${trClass-1} td.${tdClass}`).css('background-color', `${globalColor}`)
-        $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+        $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', `${color}`)
+        $(`tr.${trClass-1} td.${tdClass}`).css('background-color', `${color}`)
+        $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', `${color}`)
         //under current selected td
-        $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', `${globalColor}`)
-        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${globalColor}`)
-        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${globalColor}`)
+        $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', `${color}`)
+        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', `${color}`)
+        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', `${color}`)
       // end attempt enlarging brush size
       e.stopPropagation()
     })
   })
   $('#paintTable td').on('mouseup', function(event) {
     $('#paintTable td').off()
-    paint9()
+    brush9()
 
     event.stopPropagation()
   })
 }
 
+
+
+//PAINT BRUSH
 function brushSizeListener(){
   $('.brushes').on('mouseover', function(event){
       $('#brushMenu').show()
@@ -131,35 +142,26 @@ function brushSizeListener(){
   $('#brush1').on('click', function(event){
     event.stopPropagation()
     brushPrep('brush','1')
-    paint1()
-
+    brush1(globalColor)
   })
 
   $('#brush4').on('click', function(event){
     event.stopPropagation()
     brushPrep('brush','4')
-    paint4()
+    brush4(globalColor)
   })
 
 
   $('#brush9').on('click', function(event){
     event.stopPropagation()
     brushPrep('brush', '9')
-    paint9()
+    brush9(globalColor)
   })
+
+  //load default brush
+  brushPrep('brush','1')
+  brush1(globalColor)
 }
-
-
-//SHARED ERASER AND BRUSH CODE
-function brushPrep(action, size){
-  $('.brushSizes').removeClass('active')
-  $('.dependentTools').removeClass('active')
-  $(`#${action}MenuButton`).addClass('active')
-  $('#paintTable td').off()
-  $(`#${action}${size}`).toggleClass('active')
-  $('#paintTable').css('cursor',`url('css/cursors/${action}_cursor.png'), auto`)
-}
-
 
 //EYEDROPPER
 function eyedrop() {
@@ -185,108 +187,6 @@ function eyedropperListener(){
 }
 
 //ERASER
-
-function eraser1() {
-  $('#paintTable td').on('mousedown', function(event) {
-    event.stopPropagation()
-    $(this).css('background-color', '#ffffff')
-
-    $('#paintTable td').on('mouseover', function(e) {
-      $(this).css('background-color', '#ffffff')
-    })
-  })
-  $('#paintTable td').on('mouseup', function(event) {
-    $('#paintTable td').off()
-    eraser1()
-    event.stopPropagation()
-  })
-}
-
-function eraser4() {
-  $('#paintTable td').on('mousedown', function(event) {
-    event.stopPropagation()
-    $(this).css('background-color', '#ffffff')
-    //attempt at enalarging brush serialize
-    let tdClass = parseInt(this.className)
-    let trClass = parseInt(this.parentElement.className)
-      // //sides of current selected td
-      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', '#ffffff')
-      //under current selected td
-      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', '#ffffff')
-      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', '#ffffff')
-    // end attempt enlarging brush size
-
-    $('#paintTable td').on('mouseover', function(e) {
-      $(this).css('background-color', `#ffffff`)
-      //attempt at enalarging brush serialize
-      let tdClass = parseInt(this.className)
-      let trClass = parseInt(this.parentElement.className)
-        // //sides of current selected td
-        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', '#ffffff')
-        //under current selected td
-        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', '#ffffff')
-        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', '#ffffff')
-      // end attempt enlarging brush size
-      e.stopPropagation()
-    })
-  })
-  $('#paintTable td').on('mouseup', function(event) {
-    $('#paintTable td').off()
-    eraser4()
-    event.stopPropagation()
-  })
-}
-
-function eraser9() {
-  $('#paintTable td').on('mousedown', function(event) {
-    event.stopPropagation()
-    $(this).css('background-color', `#ffffff`)
-    //attempt at enalarging brush serialize
-    let tdClass = parseInt(this.className)
-    let trClass = parseInt(this.parentElement.className)
-      // //sides of current selected td
-      $(`tr.${trClass} td.${tdClass-1}`).css('background-color', '#ffffff')
-      $(`tr.${trClass} td.${tdClass+1}`).css('background-color', '#ffffff')
-      //above current selected td
-      $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', '#ffffff')
-      $(`tr.${trClass-1} td.${tdClass}`).css('background-color', '#ffffff')
-      $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', '#ffffff')
-      //under current selected td
-      $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', '#ffffff')
-      $(`tr.${trClass+1} td.${tdClass}`).css('background-color', '#ffffff')
-      $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', '#ffffff')
-    // end attempt enlarging brush size
-
-    $('#paintTable td').on('mouseover', function(e) {
-      $(this).css('background-color', '#ffffff')
-
-      //attempt at enalarging brush serialize
-      let tdClass = parseInt(this.className)
-      let trClass = parseInt(this.parentElement.className)
-        // //sides of current selected td
-        $(`tr.${trClass} td.${tdClass-1}`).css('background-color', '#ffffff')
-        $(`tr.${trClass} td.${tdClass+1}`).css('background-color', '#ffffff')
-        //above current selected td
-        $(`tr.${trClass-1} td.${tdClass-1}`).css('background-color', '#ffffff')
-        $(`tr.${trClass-1} td.${tdClass}`).css('background-color', '#ffffff')
-        $(`tr.${trClass-1} td.${tdClass+1}`).css('background-color', '#ffffff')
-        //under current selected td
-        $(`tr.${trClass+1} td.${tdClass-1}`).css('background-color', '#ffffff')
-        $(`tr.${trClass+1} td.${tdClass}`).css('background-color', '#ffffff')
-        $(`tr.${trClass+1} td.${tdClass+1}`).css('background-color', '#ffffff')
-      // end attempt enlarging brush size
-      e.stopPropagation()
-    })
-  })
-  $('#paintTable td').on('mouseup', function(event) {
-    $('#paintTable td').off()
-    eraser9()
-
-    event.stopPropagation()
-  })
-}
-
-
 function eraserSizeListener(){
   $('.erasers').on('mouseover', function(event){
       $('#eraserMenu').show()
@@ -299,20 +199,20 @@ function eraserSizeListener(){
   $('#eraser1').on('click', function(event){
     event.stopPropagation()
     brushPrep('eraser','1')
-    eraser1()
+    brush1('#ffffff')
   })
 
   $('#eraser4').on('click', function(event){
     event.stopPropagation()
     brushPrep('eraser','4')
-    eraser4()
+    brush4('#ffffff')
   })
 
 
   $('#eraser9').on('click', function(event){
     event.stopPropagation()
     brushPrep('eraser','9')
-    eraser9()
+    brush9('#ffffff')
   })
 }
 
