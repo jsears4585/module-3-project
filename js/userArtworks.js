1942a5e75330ffa66af763ad30bd08eabbd18095
Jason Sears <jsears4585@gmail.com>
2017-07-14 12:31:58 -0400

    Community and User page styling tweaks

--- js/userArtworks.js
+++ js/userArtworks.js
@@ -1,16 +1,16 @@
 function printTitle(data) {
   $('#userArtworksList').append('<div style="width: 20%" class="grid-sizer"></div>')
   data.forEach(function(artwork) {
-    $('#userArtworksList').append(`<div style="width: 20%" class="grid-item"><h3>${artwork.title}</h3><img src='${artwork.image}' alt='${artwork.title}'></div>`)
+    $('#userArtworksList').append(`<div style="width: 20%; margin-bottom: 55px;" class="grid-item"><h3>${artwork.title}</h3><img style="max-width: 200px;" src='${artwork.image}' alt='${artwork.title}'></div>`)
     console.log(artwork)
   })
   var $grid = $('.masonryGrid').masonry({
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     percentPosition: true,
-    gutter: 200,
+    gutter: 140,
     transitionDuration: '1s',
-    stagger: 30
+    stagger: 10
   })
   // layout Masonry after each image loads
   $grid.imagesLoaded().progress(function() {
