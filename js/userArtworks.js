c27935a4e438dd2723e17d0032d9bdf8c8767feb
Jason Sears <jsears4585@gmail.com>
2017-07-14 11:32:42 -0400

    Added userArtworks functionality

--- js/userArtworks.js
+++ js/userArtworks.js
@@ -1,28 +1,29 @@
 function printTitle(data) {
+  $('#userArtworksList').append('<div style="width: 20%" class="grid-sizer"></div>')
   data.forEach(function(artwork) {
-    $('#userArtworksList').empty()
-    $('#userArtworksList').append(`<li><a style='cursor: pointer;' data-id='${artwork.id}'>${artwork.title}</a></li>`)
+    $('#userArtworksList').append(`<div style="width: 20%" class="grid-item"><h3>${artwork.title}</h3><img src='${artwork.image}' alt='${artwork.title}'></div>`)
+    console.log(artwork)
+  })
+  var $grid = $('.masonryGrid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    gutter: 200,
+    transitionDuration: '1s',
+    stagger: 30
+  })
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress(function() {
+    $grid.masonry('layout')
   })
 }
 $(function() {
+  $('#userArtworksList').empty()
   $.ajax({
-    url: `http://localhost:3000/users/${currentUser.id}`,
+    url: `http://localhost:3000/artworks/user/${currentUser.id}`,
     type: 'get',
     crossDomain: true
   }).done(function(data) {
-    printTitle(data.artworks)
-  }).done(function() {
-    $('#userArtworksList li a').on('click', function(event) {
-      event.preventDefault()
-      let artworkID = $(this).data('id')
-      console.log('artworkID is: ' + artworkID)
-      console.log($(this))
-      $.get(`http://localhost:3000/artworks/${artworkID}`, function(data) {
-        $('#imageHolder-user').empty()
-        $('#titleHeader-user').text(data.title)
-        $('#imageHolder-user').html(data.artwork_HTML)
-        $('.ui.basic.modal.show-user-artwork').modal('show')
-      })
-    })
+    printTitle(data)
   })
 })
